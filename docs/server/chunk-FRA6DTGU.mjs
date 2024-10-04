937c18fe9510a08b6a323753c94af85d10322d37
Ashwini11jha <Jhaashwini1104@gmail.com>
2024-10-04 09:57:42 +0530

    Add docs folder

---
+++ docs/server/chunk-FRA6DTGU.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a as r,b as i,c as n,d as p,e as m,j as s,m as a,o as f,p as c,s as d,y as l}from"./chunk-LFG2YWWT.mjs";var u=(()=>{let o=class o{constructor(){this.title="rashi"}};o.\u0275fac=function(e){return new(e||o)},o.\u0275cmp=r({type:o,selectors:[["app-root"]],standalone:!0,features:[m],decls:2,vars:0,template:function(e,A){e&1&&(i(0,"div"),p(1,` This is the new App
+`),n())}});let t=o;return t})();var v=[];var g={providers:[s({eventCoalescing:!0}),l(v),c()]};var x={providers:[d()]},C=a(g,x);var y=()=>f(u,C),z=y;export{z as a};
